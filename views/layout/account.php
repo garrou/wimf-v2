fa414f2f6b15c0db54ee5f2722b027067a098fde
garrou <garrousteadrien@gmail.com>
2023-08-28 00:10:46 +0200

    Add icon

--- views/layout/account.php
+++ views/layout/account.php
@@ -8,6 +8,7 @@ use App\Auth;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <link rel="icon" href="img/logo.png" />
     <title><?= $title ?? 'WIMF' ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 </head>
